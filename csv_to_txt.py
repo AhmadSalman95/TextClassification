5f78b7349ecb4fda6ddfa2111d19c09afa1a0e37
ahmad salman <as.ahmad.salman@gmail.com>
2020-10-26 12:30:44 +0300

    modify csvTotxt file

--- csv_to_txt.py
+++ csv_to_txt.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 
 
-def csv_to_txt(inFilePath, nameOfClass, columns):
+def csv_to_txt_train_test_group(inFilePath, nameOfClass, columns):
     # split the data to train 80% and test 20%
     inFilePath = inFilePath
     nameOfClass = nameOfClass
@@ -47,4 +47,16 @@ def csv_to_txt(inFilePath, nameOfClass, columns):
     testDataset.close()
 
 
-
+def csv_full_to_txt(files_dir, classes):
+    files_dir = files_dir
+    i = 0
+    for file in os.listdir(files_dir):
+        if file.endswith('.csv'):
+            file_csv_dir = "{}/{}".format(files_dir, file)
+            text_csv = open(file_csv_dir, 'r')
+            text_csv_read = text_csv.read()
+            file_output_name = "full_text_{}.txt".format(classes[i])
+            with open(file_output_name, 'w') as out_file:
+                out_file.write(text_csv_read)
+            out_file.close()
+            i = i + 1
