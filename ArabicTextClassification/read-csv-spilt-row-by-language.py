472b37e5c5e5fb939c1097f7dd55c6cbfc23662e
ahmad salman <as.ahmad.salman@gmail.com>
2020-10-28 12:30:45 +0300

    modify the read-csv-spilt-row-by-language

--- ArabicTextClassification/read-csv-spilt-row-by-language.py
+++ ArabicTextClassification/read-csv-spilt-row-by-language.py
@@ -8,44 +8,37 @@ def append_row(file_name: str, row: str):
         csv_writer.writerow(row)
 
 
-file_dir = '/home/ahmad/Desktop/UniversityChatbotTextClassification/three-class-classification/BB-description.csv'
-out_dir_ar = '/home/ahmad/Desktop/UniversityChatbotTextClassification/three-class-classification/BB-ar-row.csv'
-out_dir_en = '/home/ahmad/Desktop/UniversityChatbotTextClassification/three-class-classification/BB-en-row.csv'
-out_dir_other = '/home/ahmad/Desktop/UniversityChatbotTextClassification/three-class-classification/BB-other-row.csv'
+file_dir = ''
+out_dir_ar = ''
+out_dir_en = ''
+out_dir_other = ''
 file = open(file_dir, 'r')
 en_row = 0
 ar_row = 0
 other_row = 0
 file_row = 0
+mistake_row = 0
 
 for row in csv.reader(file):
     try:
         lan_row = langdetect.detect(row[0])
     except:
-        file_row = file_row+1
-        print("dectect row mistack: #", file_row)
+        file_row = file_row + 1
+        print("detect row mistake: #", file_row)
+        mistake_row = mistake_row+1
     if lan_row == 'en':
         append_row(out_dir_en, row)
-        # with open(out_dir_en, 'w') as output_file:
-        #     writer = csv.writer(output_file)
-        #     writer.writerow(row)
-        # # output_file.close()
         en_row = en_row + 1
     elif lan_row == 'ar':
         append_row(out_dir_ar, row)
-        # with open(out_dir_ar, 'w') as output_file:
-        #     writer = csv.writer(output_file)
-        #     writer.writerow(row)
-        # output_file.close()
         ar_row = ar_row + 1
     else:
         append_row(out_dir_other, row)
-        # with open(out_dir_other, 'w') as output_file:
-        #     writer = csv.writer(output_file)
-        #     writer.writerow(row)
-        # output_file.close()
         other_row = other_row + 1
     file_row = file_row + 1
     print("file row : ", file_row)
 
-print("the rows is: \n en-row = {} \n ar-row = {} \n other-row = {} ".format(en_row, ar_row, other_row))
+print("the rows is: \n en-row = {} \n ar-row = {} \n other-row = {} \n mistake-row ".format(en_row,
+                                                                                            ar_row,
+                                                                                            other_row,
+                                                                                            mistake_row))
