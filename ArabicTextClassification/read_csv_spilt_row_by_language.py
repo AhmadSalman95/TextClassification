af89314980195803b87cce57eab8174633015779
ahmad salman <as.ahmad.salman@gmail.com>
2020-10-28 15:19:01 +0300

    convert the read_csv_spilt_row_by_language from file to method

--- ArabicTextClassification/read_csv_spilt_row_by_language.py
+++ ArabicTextClassification/read_csv_spilt_row_by_language.py
@@ -1,5 +1,6 @@
 import langdetect
 import csv
+import os
 
 
 def append_row(file_name: str, row: str):
@@ -8,37 +9,46 @@ def append_row(file_name: str, row: str):
         csv_writer.writerow(row)
 
 
-file_dir = ''
-out_dir_ar = ''
-out_dir_en = ''
-out_dir_other = ''
-file = open(file_dir, 'r')
-en_row = 0
-ar_row = 0
-other_row = 0
-file_row = 0
-mistake_row = 0
+def csv_spilt_to_CSVs_by_language(file_dir: str, out_dir_ar: str, out_dir_en: str, out_dir_other: str):
+    """this function spilt the csv file to csv files by row language
+    :parameter
+    file_dir: string of path csv file you want spilt
+    out_dir_ar: string of path csv file the language is arabic
+    out_dir_en: string of path csv file the language is english
+    out_dir_other : string of path csv file the language doesn't detect"""
+    file_name = os.path.basename(file_dir)
+    file_name = os.path.splitext(file_name)[0]
+    file = open(file_dir, 'r')
+    en_row = 0
+    ar_row = 0
+    other_row = 0
+    file_row = 0
+    mistake_row = 0
+    arabic_file = "{}/{}.csv".format(out_dir_ar, file_name)
+    english_file = "{}/{}.csv".format(out_dir_en, file_name)
+    other_file = "{}/{}.csv".format(out_dir_other, file_name)
 
-for row in csv.reader(file):
-    try:
-        lan_row = langdetect.detect(row[0])
-    except:
+    for row in csv.reader(file):
+        try:
+            lan_row = langdetect.detect(row[0])
+        except:
+            file_row = file_row + 1
+            print("{}: detect row mistake: #{}".format(file_name, file_row))
+            mistake_row = mistake_row + 1
+        if lan_row == 'en':
+            append_row(english_file, row)
+            en_row = en_row + 1
+        elif lan_row == 'ar':
+            append_row(arabic_file, row)
+            ar_row = ar_row + 1
+        else:
+            append_row(other_file, row)
+            other_row = other_row + 1
         file_row = file_row + 1
-        print("detect row mistake: #", file_row)
-        mistake_row = mistake_row+1
-    if lan_row == 'en':
-        append_row(out_dir_en, row)
-        en_row = en_row + 1
-    elif lan_row == 'ar':
-        append_row(out_dir_ar, row)
-        ar_row = ar_row + 1
-    else:
-        append_row(out_dir_other, row)
-        other_row = other_row + 1
-    file_row = file_row + 1
-    print("file row : ", file_row)
+        # print("file row : ", file_row)
 
-print("the rows is: \n en-row = {} \n ar-row = {} \n other-row = {} \n mistake-row ".format(en_row,
-                                                                                            ar_row,
-                                                                                            other_row,
-                                                                                            mistake_row))
+    print("the {} rows is: \n en-row = {} \n ar-row = {} \n other-row = {} \n mistake-row = {} \n".format(file_name,
+                                                                                                          en_row,
+                                                                                                          ar_row,
+                                                                                                          other_row,
+                                                                                                          mistake_row))
