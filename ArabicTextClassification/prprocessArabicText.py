f53e256fbbab9e535ef1e07740c8d068ddbc8b8e
ahmad salman <as.ahmad.salman@gmail.com>
2020-10-29 14:02:02 +0300

    add removeWeirdChars method to preprocessArabicText&visualisation

--- ArabicTextClassification/prprocessArabicText.py
+++ ArabicTextClassification/prprocessArabicText.py
@@ -1,6 +1,33 @@
 import os
 from nltk.tokenize import RegexpTokenizer
 import nltk
+import re
+
+def removeWeirdChars(text):
+    weridPatterns = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u'\U00010000-\U0010ffff'
+                               u"\u200d"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\u3030"
+                               u"\ufe0f"
+                               u"\u2069"
+                               u"\u2066"
+                               u"\u200c"
+                               u"\u2068"
+                               u"\u2067"
+                               "]+", flags=re.UNICODE)
+    return weridPatterns.sub(r'', text)
 
 
 def remove_punctuation(text: str):
@@ -20,18 +47,20 @@ def remove_stop_words(text: str, stopwords: list):
     return " ".join([word for word in text_tokens if not word in stopwords])
 
 
-def preprocess_text_in_folder(in_files_dir: str, out_files_dir: str, stopwords: list):
+def preprocess_text_in_folder(in_files_dir: str, out_files_dir: str, stop_words_dir: str):
     """this function to remove the stop words and punctuation and save the output to .txt file
     parameter:
     in_file_dir:string path input text files
     out_files_dir:string path output text files
-    stopwords : list of string words you need deleted"""
+    stopwords :string of path words you need deleted"""
+    stop_words_list = stop_words(stop_words_dir)
     for file in os.listdir(in_files_dir):
         if file.endswith(".txt"):
             file_dir = "{}/{}".format(in_files_dir, file)
             text_file = open(file_dir, 'r')
             text_file = text_file.read()
-            nostop = remove_stop_words(text_file, stopwords)
+            noWeirdChars = removeWeirdChars(text_file)
+            nostop = remove_stop_words(noWeirdChars, stop_words_list)
             nopunctuation = remove_punctuation(nostop)
             file_full_name = os.path.basename(file)
             file_name = os.path.splitext(file_full_name)[0]
@@ -41,13 +70,14 @@ def preprocess_text_in_folder(in_files_dir: str, out_files_dir: str, stopwords: 
             output_file.close()
 
 
-def preprocess_text(text:str, stop_words_dir:str):
+def preprocess_text(text: str, stop_words_dir: str):
     """this method remove stop words and the punctuation and return the text
      parameter:
      text: string of text
      stop_word_dir: string of file stop words bath"""
     stop_words_list = stop_words(stop_words_dir)
-    no_stop = remove_stop_words(text, stop_words_list)
+    noWeirdChars = removeWeirdChars(text)
+    no_stop = remove_stop_words(noWeirdChars, stop_words_list)
     no_punctuation = remove_punctuation(no_stop)
     return no_punctuation
 
--- ArabicTextClassification/visualisation.py
+++ ArabicTextClassification/visualisation.py
@@ -2,32 +2,74 @@ import arabic_reshaper
 from bidi.algorithm import get_display
 from wordcloud import WordCloud
 import os
+import re
 
 
-def visualisation_vocabulary_folder(files_dir: str, status_of_preprocess: bool):
+def removeWeirdChars(text):
+    weridPatterns = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u'\U00010000-\U0010ffff'
+                               u"\u200d"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\u3030"
+                               u"\ufe0f"
+                               u"\u2069"
+                               u"\u2066"
+                               u"\u200c"
+                               u"\u2068"
+                               u"\u2067"
+                               "]+", flags=re.UNICODE)
+    return weridPatterns.sub(r'', text)
+
+def visualisation_vocabulary_folder(files_dir: str, status_of_preprocess: bool, output_dir_images: str):
     """this method visulisation all files in folder but .txt
     parameter:
     files_dir:string of directory the files you need to visualisation_vocabulary,
-    status_of_preprocess: boolean value:true=>after,false=>before"""
+    status_of_preprocess: boolean value:true=>after,false=>before
+    output_dir_images : string of path the output direction you need to save the images """
     if status_of_preprocess:
         preprocess = 'after_preprocess'
     else:
         preprocess = 'befor_preprocess'
     for file in os.listdir(files_dir):
         if file.endswith('.txt'):
+            file_full_name = os.path.basename(file)
+            file_name = os.path.splitext(file_full_name)[0]
+            print("start : {}".format(file_name))
             text_file_bath = os.path.join(files_dir, file)
             text_file = open(text_file_bath, 'r')
             text = text_file.read()
-            text = arabic_reshaper.reshape(text)
-            text = get_display(text)
-            word_cloud = WordCloud(font_path='NotoNaskhArabic-Regular.ttf').generate(text)
-            file_full_name = os.path.basename(file)
-            file_name = os.path.splitext(file_full_name)[0]
-            output_image = "{}/{}_{}.png".format(files_dir, preprocess, file_name)
+            if not status_of_preprocess:
+                text = removeWeirdChars(text)
+                text = arabic_reshaper.reshape(text)
+                text = get_display(text)
+            else:
+                text = arabic_reshaper.reshape(text)
+                text = get_display(text)
+            word_cloud = WordCloud(width=1900, height=800, font_path='NotoNaskhArabic-Regular.ttf').generate(text)
+            output_image = "{}/{}_{}.png".format(output_dir_images, preprocess, file_name)
             word_cloud.to_file(output_image)
+            print("finish : {}".format(file_name))
             text_file.close()
 
-def visualisation_vocabulary_file(text:str, output_dir:str,name_of_output_file:str):
+
+def visualisation_vocabulary_file(text: str, output_dir: str, name_of_output_file: str):
+    """this function visualisation file
+    parameter:
+    text: string text
+    output_dir: string of path you need save image
+    name_of_output_file: string of name the image"""
+
     textArabic = arabic_reshaper.reshape(text)
     textArabic = get_display(textArabic)
     word_cloud = WordCloud(font_path='NotoNaskhArabic-Regular.ttf').generate(textArabic)
